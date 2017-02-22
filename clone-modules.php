0b7a8b0974cae84b5d7c81b4077e5e5e32f2bbb6
KennyNguyen <nguyentiendat713@gmail.com>
2017-02-22 14:15:39 +0700

    Update Clone tools
    + Add sample code.
    + Add language check.

--- clone-modules.php
+++ clone-modules.php
@@ -9,17 +9,26 @@ define('NV_ROOTDIR', pathinfo(str_replace(DIRECTORY_SEPARATOR, '/', __file__), P
 require NV_ROOTDIR . '/includes/mainfile.php';
 
 // Dien ten module goc ( news, shops, ...)
+// $module = 'news';
 $module = 'shops';
+$module = str_replace("-", "_", change_alias($module));
 
-//Dien ten module Ao cua module muon copy
+// Dien ten module Ao cua module muon copy
 // Can phai luu y chon chinh xac, neu khong se hong cau truc module
+// $clone = 'articles';
 $clone = 'product';
-
 $clone = str_replace("-", "_", change_alias($clone));
+
+// Khai bao ngon ngu data muon copy. De trong neu muon copy toan bo
+// $lang = 'en';
+$lang = 'vi';
+
+$source_module = (!empty($lang)) ? $lang . '_' . $module : $module;
+$target_module = (!empty($lang)) ? $lang . '_' . $clone : $clone;
 
 // Lay danh sach table cua module goc
 try {
-	$query = $db->query("SHOW TABLES LIKE '%_" . $module . "_%'");
+	$query = $db->query("SHOW TABLES LIKE '%_" . $source_module . "_%'");
 	$tables = $query->fetchAll(3);
 	foreach( $tables as $table){
 		$target_table = str_replace($module, $clone, $table[0]);
@@ -42,3 +51,4 @@ try {
 } catch (PDOException $e) {
 	trigger_error($e->getMessage());
 }
+die('Clone successful ! Please check all error(s) ouput and delete this file !');
